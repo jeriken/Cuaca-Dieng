51679906944987989e7be9d0c0b9b8d075bb5ead
Jeriken <mrazin777@gmail.com>
2024-06-03 07:06:43 +0700

    feat: Adding header side content

---
+++ src/components/Header.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="flex justify-between mb-8 items-center">
+        <div>
+            <h1 class="font-sans font-semibold text-2xl text-gray-700">Rekap Cuaca hari ini</h1>
+        </div>
+        <div>
+            <h1 class="font-sans font-semibold text-2xl text-blue-500 text-right tracking-wide">09:30</h1>
+            <h1 class="font-sans font-semibold text-sm text-gray-700 text-right">19 April 2024</h1>
+        </div>
+    </div>    
+</template>
