86a0f2dff445727824220868882d078a9df12c47
Jeriken <mrazin777@gmail.com>
2025-06-20 14:02:31 +0700

    fix: Fix bug on mounted

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref, onBeforeMount } from 'vue'
+import { ref, onMounted } from 'vue'
 import axios from 'axios'
 import moment from 'moment/min/moment-with-locales'
 import Main from '../components/Main.vue'
@@ -45,15 +45,20 @@ const callApi = async (url) => {
     return data
 }
 
-onBeforeMount(async () => {
+onMounted(async () => {
     await getMainData()
     await changeTime(1, 60, false)
+    setInterval(async () => {
+        await getMainData()
+        dateNow.value = moment().format('DD MMMM YYYY')
+        timeNow.value = moment().format('HH:mm')
+    }, 60000)
 })
 </script>
 
 <template>
     <div class="grid grid-cols-11">
-        <div class="col-span-11 md:col-span-5 lg:col-span-4 xl:col-span-3 md:h-screen md:sticky top-0 bg-stone-50 pb-8">
+        <div class="col-span-11 md:col-span-5 lg:col-span-4 xl:col-span-3 md:h-screen md:sticky top-0 bg-stone-50">
             <Main :data="mainData" />
         </div>
         <div class="col-span-11 md:col-span-6 lg:col-span-7 xl:col-span-8 bg-stone-100 p-8">
